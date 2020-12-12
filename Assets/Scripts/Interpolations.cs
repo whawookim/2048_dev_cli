d8288ac619e9440af0bb85e0f8e2dd38f523beb1
whawoo.kim <greedviolet@gmail.com>
2020-12-13 06:47:38 +0900

    UIAnimations 새로 추가 및 Interpolations 업데이트

--- Assets/Scripts/Interpolations.cs
+++ Assets/Scripts/Interpolations.cs
@@ -2,10 +2,147 @@ using System;
 
 public static class Interpolations
 {
-	public delegate float InterpolationsAction(float t, float b, float c, float d);
+	public delegate double InterpolationsAction(double t, double b, double c, double d);
 	
-	public static float Linear(float t, float b, float c, float d)
+	public static double Linear(double t, double b, double c, double d)
 	{
 		return c * t / d + b;
+	}
+
+	public static double EaseInQuad(double t, double b, double c, double d)
+	{
+		t /= d;
+		return c * t * t + b;
+	}
+
+	public static double EaseOutQuad(double t, double b, double c, double d)
+	{
+		t /= d;
+		return -c * t * (t - 2) + b;
+	}
+
+	public static double EaseInOutQuad(double t, double b, double c, double d)
+	{
+		t /= d / 2;
+		if (t < 1) return c / 2 * t * t + b;
+		t--;
+		return -c / 2 * ( t * (t - 2) - 1) + b;
+	}
+
+	public static double EaseInCubic(double t, double b, double c, double d)
+	{
+		t /= d;
+		return c * t * t * t + b;
+	}
+
+	public static double EaseOutCubic(double t, double b, double c, double d)
+	{
+		t /= d;
+		t--;
+		return c * (t * t * t + 1) + b;
+	}
+
+	public static double EaseInOutCubic(double t, double b, double c, double d)
+	{
+		t /= d / 2;
+		if (t < 1) return c / 2 * t * t * t + b;
+		t -= 2;
+		return c / 2 * (t * t * t + 2) + b;
+	}
+
+	public static double EaseInQuart(double t, double b, double c, double d)
+	{
+		t /= d;
+		return c * t * t * t * t + b;
+	}
+
+	public static double EaseOutQuart(double t, double b, double c, double d)
+	{
+		t /= d;
+		t--;
+		return -c * (t * t * t * t - 1) + b;
+	}
+
+	public static double EaseInOutQuart(double t, double b, double c, double d)
+	{
+		t /= d / 2;
+		if (t < 1) return c / 2 * t * t * t * t + b;
+		t -= 2;
+		return -c / 2 * (t * t * t * t - 2) + b;
+	}
+
+	public static double EaseInQuint(double t, double b, double c, double d)
+	{
+		t /= d;
+		return c * t * t * t * t * t + b;
+	}
+
+	public static double EaseOutQuint(double t, double b, double c, double d)
+	{
+		t /= d;
+		t--;
+		return c * (t * t * t * t * t + 1) + b;
+	}
+
+	public static double EaseInOutQuint(double t, double b, double c, double d)
+	{
+		t /= d / 2;
+		if (t < 1) return c / 2 * t * t * t * t * t + b;
+		t -= 2;
+		return c / 2 * (t * t * t * t * t + 2) + b;
+	}
+
+	public static double EaseInSine(double t, double b, double c, double d)
+	{
+		return -c * Math.Cos(t / d * (Math.PI / 2)) + c + b;
+	}
+
+	public static double EaseOutSine(double t, double b, double c, double d)
+	{
+		return c * Math.Sin(t / d * (Math.PI / 2)) + b;
+	}
+
+	public static double EaseInOutSine(double t, double b, double c, double d)
+	{
+		return -c / 2 * (Math.Cos(Math.PI * t / d) - 1) + b;
+	}
+
+	public static double EaseInExpo(double t, double b, double c, double d)
+	{
+		return c * Math.Pow( 2, 10 * (t / d - 1) ) + b;
+	}
+
+	public static double EaseOutExpo(double t, double b, double c, double d)
+	{
+		return c * ( -Math.Pow( 2, -10 * t / d ) + 1 ) + b;
+	}
+
+	public static double EaseInOutExpo(double t, double b, double c, double d)
+	{
+		t /= d / 2;
+		if (t < 1) return c / 2 * Math.Pow( 2, 10 * (t - 1) ) + b;
+		t--;
+		return c/2 * ( -Math.Pow( 2, -10 * t) + 2 ) + b;
+	}
+
+	public static double EaseInCirc(double t, double b, double c, double d)
+	{
+		t /= d;
+		return -c * (Math.Sqrt(1 - t * t) - 1) + b;
+	}
+
+	public static double EaseOutCirc(double t, double b, double c, double d)
+	{
+		t /= d;
+		t--;
+		return c * Math.Sqrt(1 - t * t) + b;
+	}
+
+	public static double EaseInOutCirc(double t, double b, double c, double d)
+	{
+		t /= d / 2;
+		if (t < 1) return -c / 2 * (Math.Sqrt(1 - t * t) - 1) + b;
+		t -= 2;
+		return c / 2 * (Math.Sqrt(1 - t * t) + 1) + b;
 	}
 }
--- Assets/Scripts/UIAnimations.cs
+++ Assets/Scripts/UIAnimations.cs
@@ -1,28 +1,91 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public static class UIAnimations
 {
 	public static IEnumerator Position(UIRect widget, float duration, Vector3 targetPos, 
-		Interpolations.InterpolationsAction interpolationsAction)
+		Interpolations.InterpolationsAction interpolationsAction, bool isLocal = false)
 	{
-		var startPos = widget.cachedTransform.localPosition;
+		var startPos = (isLocal) ? widget.cachedTransform.localPosition : widget.cachedTransform.position;
 		var sumTime = 0.0f;
 
 		while (sumTime < duration)
 		{
-			widget.cachedTransform.localPosition =
-				Vector3.Lerp(startPos, targetPos,
-					interpolationsAction.Invoke(sumTime, startPos.sqrMagnitude, (targetPos - startPos).sqrMagnitude,
-						duration));
+			var tempPos = Vector3.Lerp(startPos, targetPos,
+				(float) interpolationsAction.Invoke(sumTime, 0, 1, duration));
 
+			if (isLocal)
+			{
+				widget.cachedTransform.localPosition = tempPos;
+			}
+			else
+			{
+				widget.cachedTransform.position = tempPos;
+			}
+			
 			yield return null;
 
 			sumTime += Time.deltaTime;
 		}
 
-		widget.cachedTransform.localPosition = targetPos;
-		
-		yield break;
+		if (isLocal)
+		{
+			widget.cachedTransform.localPosition = targetPos;
+		}
+		else
+		{
+			widget.cachedTransform.position = targetPos;
+		}
+	}
+
+	public static IEnumerator Alpha(UIRect rect, float duration, float targetAlpha, 
+		Interpolations.InterpolationsAction interpolationsAction)
+	{
+		var startAlpha = rect.alpha;
+		var sumTime = 0.0f;
+
+		while (sumTime < duration)
+		{
+			rect.alpha = Mathf.Lerp(startAlpha, targetAlpha, 
+				(float)interpolationsAction.Invoke(sumTime, 0, 1, duration));
+			
+			yield return null;
+
+			sumTime += Time.deltaTime;
+		}
+
+		rect.alpha = targetAlpha;
+	}
+
+	public static IEnumerator Scale(Transform transform, float duration, Vector3 targetScale,
+		Interpolations.InterpolationsAction interpolationsAction)
+	{
+		var startScale = transform.localScale;
+		var sumTime = 0.0f;
+
+		while (sumTime < duration)
+		{
+			transform.localScale = Vector3.Lerp(startScale, targetScale,
+				(float)interpolationsAction.Invoke(sumTime, 0, 1, duration));
+			
+			yield return null;
+
+			sumTime += Time.deltaTime;
+		}
+
+		transform.localScale = targetScale;
+	}
+
+	/// <summary>
+	/// 스케일이 특정 scale까지 바뀌었다가 돌아옴
+	/// </summary>
+	public static IEnumerator ElasticScale(Transform transform, float duration, Vector3 centerScale,
+		Interpolations.InterpolationsAction increaseInterpolation, Interpolations.InterpolationsAction decreaseInterpolation)
+	{
+		var originScale = transform.localScale;
+
+		yield return Scale(transform, duration * 0.5f, centerScale, increaseInterpolation);
+		yield return Scale(transform, duration * 0.5f, originScale, decreaseInterpolation);
 	}
 }
