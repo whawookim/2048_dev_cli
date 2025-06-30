330a2d8564b451e5851b4490a4667ee23af04fa5
whawoo.kim <greedviolet@gmail.com>
2025-07-01 07:55:23 +0900

    RankingPopup 프리펩 폰트 Empty로 변경 및 FontManager 개선 작업 (Fallback 관련 구조 개선 및 핑크색으로 뜨는 버그 수정)

--- Assets/Editor/FontManagerEditor.cs
+++ Assets/Editor/FontManagerEditor.cs
@@ -1,204 +1,88 @@
-using System.Collections.Generic;
-using System.Linq;
 using UnityEditor;
-using UnityEditor.AddressableAssets.Settings;
 using UnityEngine;
-using UnityEngine.Localization;
-using UnityEngine.Localization.Settings;
-using TMPro;
-using UnityEditor.AddressableAssets;
 
 [CustomEditor(typeof(FontManager))]
 public class FontManagerEditor : Editor
 {
-    private SerializedProperty uiBaseFontProp;
-    private SerializedProperty contentBaseFontProp;
-    private SerializedProperty uiFontLabelMappingsProp;
-    private SerializedProperty contentFontLabelsProp;
-
-    private string[] availableLocales;
-    private string[] fontLabels;
+    SerializedProperty uiBaseFont;
+    SerializedProperty contentBaseFont;
+    SerializedProperty localeFontMappings;
 
     private void OnEnable()
     {
-        uiBaseFontProp = serializedObject.FindProperty("uiBaseFont");
-        contentBaseFontProp = serializedObject.FindProperty("contentBaseFont");
-        uiFontLabelMappingsProp = serializedObject.FindProperty("uiFontLabelMappings");
-        contentFontLabelsProp = serializedObject.FindProperty("contentFontLabels");
-
-        RefreshLocales();
-        RefreshFontLabels();
-        SyncLocalesWithMappings();
+        uiBaseFont = serializedObject.FindProperty("uiBaseFont");
+        contentBaseFont = serializedObject.FindProperty("contentBaseFont");
+        localeFontMappings = serializedObject.FindProperty("localeFontMappings");
     }
 
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
 
+        EditorGUILayout.Space();
         EditorGUILayout.LabelField("Font Manager", EditorStyles.boldLabel);
         EditorGUILayout.Space();
 
-        DrawBaseFonts();
-        EditorGUILayout.Space(10);
+        // 기본 폰트 설정
+        EditorGUILayout.PropertyField(uiBaseFont, new GUIContent("UI Base Font"));
+        EditorGUILayout.PropertyField(contentBaseFont, new GUIContent("Content Base Font"));
 
-        DrawUIFonts();
         EditorGUILayout.Space(10);
+        EditorGUILayout.LabelField("Fallback Fonts Per Locale", EditorStyles.boldLabel);
+        EditorGUILayout.Space(5);
 
-        DrawContentFonts();
-        EditorGUILayout.Space(10);
-
-        if (GUILayout.Button("Refresh Label & Locale List"))
+        // Locale별 Fallback 폰트 설정
+        for (int i = 0; i < localeFontMappings.arraySize; i++)
         {
-            RefreshLocales();
-            RefreshFontLabels();
-            SyncLocalesWithMappings();
-        }
-
-        serializedObject.ApplyModifiedProperties();
-    }
-
-    private void DrawBaseFonts()
-    {
-        EditorGUILayout.PropertyField(uiBaseFontProp, new GUIContent("Base UI Font (Empty)"));
-        EditorGUILayout.PropertyField(contentBaseFontProp, new GUIContent("Base Content Font (Empty)"));
-    }
-
-    private void DrawUIFonts()
-    {
-        EditorGUILayout.LabelField("UI Font Label Mappings", EditorStyles.boldLabel);
-
-        for (int i = 0; i < uiFontLabelMappingsProp.arraySize; i++)
-        {
-            var element = uiFontLabelMappingsProp.GetArrayElementAtIndex(i);
-
-            var localeProp = element.FindPropertyRelative("locale");
-            var labelListProp = element.FindPropertyRelative("addressableLabels");
+            SerializedProperty mapping = localeFontMappings.GetArrayElementAtIndex(i);
+            SerializedProperty localeCode = mapping.FindPropertyRelative("localeCode");
+            SerializedProperty fallbackFonts = mapping.FindPropertyRelative("fallbackFonts");
 
             EditorGUILayout.BeginVertical("box");
 
-            EditorGUILayout.LabelField($"Locale: {localeProp.stringValue}");
+            // Locale Code와 삭제 버튼
+            EditorGUILayout.BeginHorizontal();
+            localeCode.stringValue = EditorGUILayout.TextField("Locale Code", localeCode.stringValue);
 
-            EditorGUILayout.LabelField("Font Labels (Fallback 순서)");
-
-            for (int j = 0; j < labelListProp.arraySize; j++)
+            if (GUILayout.Button("-", GUILayout.Width(24)))
             {
-                var labelProp = labelListProp.GetArrayElementAtIndex(j);
+                localeFontMappings.DeleteArrayElementAtIndex(i);
+                break;
+            }
+            EditorGUILayout.EndHorizontal();
 
+            // 폰트 리스트
+            EditorGUILayout.LabelField("Fallback Fonts", EditorStyles.miniBoldLabel);
+
+            for (int j = 0; j < fallbackFonts.arraySize; j++)
+            {
                 EditorGUILayout.BeginHorizontal();
-                int labelIndex = Mathf.Max(0, fontLabels.ToList().FindIndex(x => x == labelProp.stringValue));
-                labelIndex = EditorGUILayout.Popup(labelIndex, fontLabels);
-                labelProp.stringValue = fontLabels[labelIndex];
 
-                if (GUILayout.Button("-", GUILayout.Width(20)))
+                EditorGUILayout.PropertyField(fallbackFonts.GetArrayElementAtIndex(j), GUIContent.none);
+
+                if (GUILayout.Button("-", GUILayout.Width(24)))
                 {
-                    labelListProp.DeleteArrayElementAtIndex(j);
-                    EditorGUILayout.EndHorizontal();
-                    continue;
+                    fallbackFonts.DeleteArrayElementAtIndex(j);
+                    break;
                 }
+
                 EditorGUILayout.EndHorizontal();
             }
 
-            if (GUILayout.Button("+ Add Label"))
+            if (GUILayout.Button("+ Add Font"))
             {
-                labelListProp.InsertArrayElementAtIndex(labelListProp.arraySize);
-                var newLabel = labelListProp.GetArrayElementAtIndex(labelListProp.arraySize - 1);
-                newLabel.stringValue = fontLabels.FirstOrDefault() ?? "font-none";
+                fallbackFonts.InsertArrayElementAtIndex(fallbackFonts.arraySize);
             }
 
             EditorGUILayout.EndVertical();
+            EditorGUILayout.Space(5);
         }
-    }
 
-    private void DrawContentFonts()
-    {
-        EditorGUILayout.LabelField("Content Font Labels (Global)", EditorStyles.boldLabel);
-
-        for (int i = 0; i < contentFontLabelsProp.arraySize; i++)
+        if (GUILayout.Button("+ Add Locale"))
         {
-            var labelProp = contentFontLabelsProp.GetArrayElementAtIndex(i);
-
-            EditorGUILayout.BeginHorizontal();
-            int labelIndex = Mathf.Max(0, fontLabels.ToList().FindIndex(x => x == labelProp.stringValue));
-            labelIndex = EditorGUILayout.Popup(labelIndex, fontLabels);
-            labelProp.stringValue = fontLabels[labelIndex];
-
-            if (GUILayout.Button("-", GUILayout.Width(20)))
-            {
-                contentFontLabelsProp.DeleteArrayElementAtIndex(i);
-                EditorGUILayout.EndHorizontal();
-                continue;
-            }
-            EditorGUILayout.EndHorizontal();
+            localeFontMappings.InsertArrayElementAtIndex(localeFontMappings.arraySize);
         }
 
-        if (GUILayout.Button("+ Add Label"))
-        {
-            contentFontLabelsProp.InsertArrayElementAtIndex(contentFontLabelsProp.arraySize);
-            var newLabel = contentFontLabelsProp.GetArrayElementAtIndex(contentFontLabelsProp.arraySize - 1);
-            newLabel.stringValue = fontLabels.FirstOrDefault() ?? "font-none";
-        }
-    }
-
-    private void SyncLocalesWithMappings()
-    {
-        var currentLocales = availableLocales.ToList();
-
-        List<string> existingLocales = new();
-        for (int i = 0; i < uiFontLabelMappingsProp.arraySize; i++)
-        {
-            var element = uiFontLabelMappingsProp.GetArrayElementAtIndex(i);
-            existingLocales.Add(element.FindPropertyRelative("locale").stringValue);
-        }
-
-        foreach (var locale in currentLocales)
-        {
-            if (!existingLocales.Contains(locale))
-            {
-                uiFontLabelMappingsProp.InsertArrayElementAtIndex(uiFontLabelMappingsProp.arraySize);
-                var newElement = uiFontLabelMappingsProp.GetArrayElementAtIndex(uiFontLabelMappingsProp.arraySize - 1);
-                newElement.FindPropertyRelative("locale").stringValue = locale;
-                var labels = newElement.FindPropertyRelative("addressableLabels");
-                labels.ClearArray();
-                labels.InsertArrayElementAtIndex(0);
-                labels.GetArrayElementAtIndex(0).stringValue = $"font-{locale}";
-            }
-        }
-    }
-
-    private void RefreshLocales()
-    {
-        availableLocales = LocalizationSettings.AvailableLocales?.Locales
-            .Select(l => l.Identifier.ToString()).ToArray();
-
-        if (availableLocales == null || availableLocales.Length == 0)
-            availableLocales = new[] { "en" };
-    }
-
-    private void RefreshFontLabels()
-    {
-        var settings = AddressableAssetSettingsDefaultObject.Settings;
-        if (settings != null)
-        {
-            var labelSet = new HashSet<string>();
-
-            foreach (var group in settings.groups)
-            {
-                foreach (var entry in group.entries)
-                {
-                    foreach (var label in entry.labels)
-                    {
-                        if (label.StartsWith("font-"))
-                            labelSet.Add(label);
-                    }
-                }
-            }
-
-            fontLabels = labelSet.ToArray();
-        }
-
-        if (fontLabels == null || fontLabels.Length == 0)
-        {
-            fontLabels = new[] { "font-none" };
-        }
+        serializedObject.ApplyModifiedProperties();
     }
 }
--- Assets/Scripts/Manager/FontManager.cs
+++ Assets/Scripts/Manager/FontManager.cs
@@ -1,166 +1,169 @@
-using System;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Linq;
+using System.Threading.Tasks;
 using TMPro;
+using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.Localization;
 using UnityEngine.Localization.Settings;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class FontManager : MonoBehaviour
 {
-    public static FontManager Instance { get; private set; }
+    [Header("Base Fonts")]
+    public TMP_FontAsset uiBaseFont;
+    public TMP_FontAsset contentBaseFont;
 
-    [Header("UI Base Font (Empty)")]
-    [SerializeField]
-    private TMP_FontAsset uiBaseFont;
-
-    [Header("Content Base Font (Empty)")]
-    [SerializeField]
-    private TMP_FontAsset contentBaseFont;
-
-    [Header("UI Font Label Mappings")]
-    [SerializeField]
-    private List<FontLabelMapping> uiFontLabelMappings;
-
-    [Header("Content Font Labels")]
-    [SerializeField]
-    private List<string> contentFontLabels;
-
-    private List<TMP_FontAsset> currentUIFallbacks = new();
-    private List<TMP_FontAsset> currentContentFallbacks = new();
-
-    private List<AsyncOperationHandle<TMP_FontAsset>> handles = new();
-
-    public TMP_FontAsset UIFont => uiBaseFont;
-    public TMP_FontAsset ContentFont => contentBaseFont;
-
-    [Serializable]
-    public struct FontLabelMapping
+    [System.Serializable]
+    public class LocaleFontMapping
     {
-        public string locale;
-        public List<string> addressableLabels;
+        public string localeCode;
+        public List<AssetReferenceT<TMP_FontAsset>> fallbackFonts;
     }
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
+    [Header("Fallback Fonts Per Locale")]
+    public List<LocaleFontMapping> localeFontMappings = new();
 
-            LocalizationSettings.SelectedLocaleChanged += OnLocaleChanged;
-            InitializeFonts();
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+    private Dictionary<string, List<TMP_FontAsset>> _localeFontCache = new();
+    private List<AsyncOperationHandle<TMP_FontAsset>> _runtimeHandles = new();
+
+    private async void Awake()
+    {
+        LocalizationSettings.SelectedLocaleChanged += OnLocaleChanged;
+
+        string currentLocale = LocalizationSettings.SelectedLocale?.Identifier.Code ?? "en";
+        await ApplyFontFallbacks(currentLocale);
     }
 
     private void OnDestroy()
     {
         LocalizationSettings.SelectedLocaleChanged -= OnLocaleChanged;
-        ReleaseAllFonts();
-    }
 
-    private void OnLocaleChanged(Locale locale)
-    {
-        LoadUIFont(locale);
-    }
-
-    private void InitializeFonts()
-    {
-        LoadUIFont(LocalizationSettings.SelectedLocale);
-        LoadContentFont();
-    }
-
-    /// <summary>
-    /// UI Font
-    /// </summary>
-    private void LoadUIFont(Locale locale)
-    {
-        var mapping = uiFontLabelMappings.Find(m => m.locale == locale.Identifier.ToString());
-        if (mapping.addressableLabels == null || mapping.addressableLabels.Count == 0)
+        // 런타임 핸들 해제
+        foreach (var handle in _runtimeHandles)
         {
-            Debug.LogWarning($"No UI font mapping for {locale.Identifier}, fallback to English.");
-            mapping = uiFontLabelMappings.Find(m => m.locale == "en");
+            Addressables.Release(handle);
         }
 
-        ReleaseCurrentFonts(currentUIFallbacks);
-        currentUIFallbacks.Clear();
+        _runtimeHandles.Clear();
+    }
 
-        foreach (var label in mapping.addressableLabels)
+    private async void OnLocaleChanged(UnityEngine.Localization.Locale locale)
+    {
+        await ApplyFontFallbacks(locale.Identifier.Code);
+    }
+
+    public async Task ApplyFontFallbacks(string locale)
+    {
+        Debug.Log($"[FontManager] Applying fallback for locale: {locale}");
+
+        List<TMP_FontAsset> fallbackList = await LoadFallbackFontsForLocale(locale);
+
+        ApplyFallbackToFont(uiBaseFont, fallbackList);
+        ApplyFallbackToFont(contentBaseFont, fallbackList);
+        ApplyFallbackToTMPSettings(fallbackList);
+    }
+
+    private async Task<List<TMP_FontAsset>> LoadFallbackFontsForLocale(string locale)
+    {
+        if (_localeFontCache.TryGetValue(locale, out var cached))
+            return cached;
+
+        var mapping = localeFontMappings.FirstOrDefault(x => x.localeCode == locale);
+        if (mapping == null)
         {
-            var handle = Addressables.LoadAssetAsync<TMP_FontAsset>(label);
-            handles.Add(handle);
+            Debug.LogWarning($"[FontManager] No fallback fonts found for locale: {locale}");
+            return new List<TMP_FontAsset>();
+        }
 
-            handle.Completed += h =>
+        var result = new List<TMP_FontAsset>();
+
+        foreach (var reference in mapping.fallbackFonts)
+        {
+            var font = LoadFontInEditorOrRuntime(reference);
+            if (font != null)
             {
-                if (h.Status == AsyncOperationStatus.Succeeded)
-                {
-                    currentUIFallbacks.Add(h.Result);
-                    ApplyUIFallback();
-                }
-                else
-                {
-                    Debug.LogError($"Failed to load font for label {label}");
-                }
-            };
+                result.Add(font);
+            }
         }
+
+        _localeFontCache[locale] = result;
+        return result;
     }
 
-    private void ApplyUIFallback()
+    private TMP_FontAsset LoadFontInEditorOrRuntime(AssetReferenceT<TMP_FontAsset> reference)
     {
-        if (uiBaseFont != null)
+#if UNITY_EDITOR
+        string guid = reference.AssetGUID;
+        string path = AssetDatabase.GUIDToAssetPath(guid);
+        TMP_FontAsset font = AssetDatabase.LoadAssetAtPath<TMP_FontAsset>(path);
+
+        if (font != null)
         {
-            uiBaseFont.fallbackFontAssetTable = currentUIFallbacks;
+            Debug.Log($"[FontManager] (Editor) Loaded font: {font.name}");
+            FixFontMaterial(font);
+            return font;
         }
-    }
-
-    /// <summary>
-    /// Content Font
-    /// </summary>
-    private void LoadContentFont()
-    {
-        ReleaseCurrentFonts(currentContentFallbacks);
-        currentContentFallbacks.Clear();
-
-        foreach (var label in contentFontLabels)
+        else
         {
-            var handle = Addressables.LoadAssetAsync<TMP_FontAsset>(label);
-            handles.Add(handle);
-
-            handle.Completed += h =>
-            {
-                if (h.Status == AsyncOperationStatus.Succeeded)
-                {
-                    currentContentFallbacks.Add(h.Result);
-                    ApplyContentFallback();
-                }
-                else
-                {
-                    Debug.LogError($"Failed to load content font for label {label}");
-                }
-            };
+            Debug.LogWarning($"[FontManager] (Editor) Failed to load font via AssetDatabase: {reference.RuntimeKey}");
+            return null;
         }
-    }
+#else
+        var handle = reference.LoadAssetAsync();
+        handle.WaitForCompletion();
 
-    private void ApplyContentFallback()
-    {
-        if (contentBaseFont != null)
+        if (handle.Status == AsyncOperationStatus.Succeeded)
         {
-            contentBaseFont.fallbackFontAssetTable = currentContentFallbacks;
+            _runtimeHandles.Add(handle);
+            FixFontMaterial(handle.Result);
+            Debug.Log($"[FontManager] (Runtime) Loaded font: {handle.Result.name}");
+            return handle.Result;
         }
+        else
+        {
+            Debug.LogError($"[FontManager] (Runtime) Failed to load font: {reference.RuntimeKey}");
+            return null;
+        }
+#endif
     }
 
-    private void ReleaseAllFonts()
+    private void ApplyFallbackToFont(TMP_FontAsset baseFont, List<TMP_FontAsset> fallbacks)
     {
-        ReleaseCurrentFonts(currentUIFallbacks);
-        ReleaseCurrentFonts(currentContentFallbacks);
+        if (baseFont == null)
+        {
+            Debug.LogWarning("[FontManager] Base font is null.");
+            return;
+        }
+
+        baseFont.fallbackFontAssetTable = fallbacks;
+        Debug.Log($"[FontManager] Applied fallback to {baseFont.name}: {string.Join(", ", fallbacks.Select(f => f.name))}");
     }
 
-    private void ReleaseCurrentFonts(List<TMP_FontAsset> fallbackList)
+    private void ApplyFallbackToTMPSettings(List<TMP_FontAsset> fallbacks)
     {
-        fallbackList.Clear();
+        TMP_Settings.fallbackFontAssets.Clear();
+        TMP_Settings.fallbackFontAssets.AddRange(fallbacks);
+        Debug.Log($"[FontManager] TMP_Settings fallback applied: {string.Join(", ", fallbacks.Select(f => f.name))}");
+    }
+
+    private void FixFontMaterial(TMP_FontAsset fontAsset)
+    {
+        if (fontAsset == null) return;
+
+        if (fontAsset.material == null)
+        {
+            fontAsset.material = new Material(Shader.Find("TextMeshPro/Distance Field"));
+            Debug.LogWarning($"Material가 null이어서 새로 생성: {fontAsset.name}");
+        }
+
+        if (fontAsset.material.mainTexture == null)
+        {
+            fontAsset.material.mainTexture = fontAsset.atlasTexture;
+            Debug.LogWarning($"mainTexture가 없어서 atlas로 연결함: {fontAsset.name}");
+        }
     }
 }
